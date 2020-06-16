0405a93cc25b3f089427497d21e9967759e07fa5
thanhpt0105 <thanhpt0105@gmail.com>
2020-06-16 14:47:38 +0800

    Spring Xml Configuration example

--- src/main/java/com/thanhpt0105/springjokeapp/SpringJokeAppApplication.java
+++ src/main/java/com/thanhpt0105/springjokeapp/SpringJokeAppApplication.java
@@ -2,8 +2,10 @@ package com.thanhpt0105.springjokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml") //example of Spring xml configuration bean.
 public class SpringJokeAppApplication {
 
     public static void main(String[] args) {
