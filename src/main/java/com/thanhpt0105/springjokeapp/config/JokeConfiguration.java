b32139c201a4ddeb2fd3f96e8fe21a42237b760b
thanhpt0105 <thanhpt0105@gmail.com>
2020-06-16 14:40:40 +0800

    Spring Java Configuration example

---
+++ src/main/java/com/thanhpt0105/springjokeapp/config/JokeConfiguration.java
@@ -0,0 +1,14 @@
+package com.thanhpt0105.springjokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokeConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes(); //Sample config class as a bean to inject in constructor.
+    }
+}
